4023b26c9d09486e325028bbd735f7b74afacbb9
Sophia Yougin <sophiya273@gmail.com>
2020-12-20 19:51:21 +0530

    added copy button

--- src/App.js
+++ src/App.js
@@ -3,44 +3,82 @@ import {Button, Form, Col} from 'react-bootstrap';
 import './App.css';
 import axios from 'axios';
 class App extends React.Component {  
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
     this.state ={
       url: '',
-      shortenedUrl: ''
+      shortenedUrl: '',
+      isCopied: false
     };
   }
-  sendRequest =(url) => {
+  getUrl = (e) =>{
+    console.log(e.target.value);
+    this.setState({
+      url:e.target.value
+    });
+  }
+  sendRequest =() =>{
     axios.post('https://cors-anywhere.herokuapp.com/https://cleanuri.com/api/v1/shorten',
-     {url: url}
-    )
-    .then((response)=>{
+     {url: this.state.url})
+     .then((response) => {
       this.setState({
       shortenedUrl: response.data.result_url
     })})
-    .catch(function (error){
-      console.log(error);
+    .catch((error) =>{
+      this.setState({
+        shortenedUrl: "Invalid URL"
+      });
     });
   }
+ copy = ()=>{
+   //select();
+   document.execCommand('copy');
+   this.setState({
+     isCopied: true
+   })
+ }
   render(){
+   console.log(this.state.isCopied);
+    function Heading(){
+      return  <Form.Label>Paste a link to shorten it</Form.Label>;
+    }
+    function ShortenButton(props){
+      return <Button variant="primary"
+                     onClick={props.click}>
+                     Shorten
+              </Button>;
+    }
+    function Result(props){
+      return <Form.Label style={props.style}>{props.shortUrl}</Form.Label>;
+    }
+    function CopyButton(props){
+      return <Button variant="outline-success" onClick={props.onClick}>Copy</Button>;
+    }
+    var resultStyle = {
+    };
+    this.state.shortenedUrl==='Invalid URL'? resultStyle.color = 'red': resultStyle.color = 'black';
       return(
-      <div>
+      <div className='container'>
         <Form.Row className='row-1'>
-        <Form.Label>Paste a link to shorten it</Form.Label>
+          <Heading/>
         </Form.Row>
         <Form.Row className='row-2'>
           <Col sm={5}>
-            <Form.Control size='lg'type="text" placeholder="Original URL"  onChange={(e)=>{this.setState({url:e.target.value})}}/>  
+            <Form.Control 
+                          size='lg'
+                          type="text"
+                          placeholder="Original URL"
+                          onChange={(e)=>this.getUrl(e)}
+            />
           </Col>
-          <Button variant="primary" type="submit" onClick={()=>this.sendRequest(this.state.url)}>
-            Shorten
-          </Button>
+          <ShortenButton click={this.sendRequest}/>
         </Form.Row>
         <Form.Row className='row-3'>
-          {this.state.shortenedUrl}
+          <Result shortUrl={this.state.shortenedUrl} style={resultStyle}/><br/>
+          {this.state.shortenedUrl!=='Invalid URL'?this.state.shortenedUrl===''?<div/>:<CopyButton onClick={this.copy}/>:<div/>}
         </Form.Row>
       </div>
-      );
-    }
-   }  
-   export default App;
+    );
+  }
+}  
+export default App;
